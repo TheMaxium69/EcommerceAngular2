ee876d68f44946ea4385b33ef0eebb13744676c1
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-05-12 11:03:51 +0200

    one delete
    one buttom delete supplier

--- src/app/supplier-list/supplier-list.component.ts
+++ src/app/supplier-list/supplier-list.component.ts
@@ -3,6 +3,7 @@ import {Product} from '../model/Product';
 import {Supplier} from '../model/Supplier';
 import {SupplierService} from '../supplier.service';
 import {SupplierHttpService} from '../supplier-http.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-supplier-list',
@@ -15,7 +16,9 @@ export class SupplierListComponent implements OnInit {
 
   selectedSupplier: Supplier;
 
-  constructor(private supplierService: SupplierHttpService) { }
+  constructor(private supplierService: SupplierHttpService,
+              private pService: SupplierHttpService,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.supplierService.findAll().subscribe( r => this.suppliers = r);
@@ -28,5 +31,14 @@ export class SupplierListComponent implements OnInit {
 
   close(): void {
     this.selectedSupplier = undefined;
+  }
+
+  deleteSupp(): void{
+
+    const result = window.confirm ('voulez vous vraiment supprimer ' + this.selectedSupplier.companyName + ' ?');
+    if(result === true){
+
+      this.pService.delete(this.selectedSupplier.id).subscribe(v => this.router.navigate(['/']));
+    }
   }
 }
